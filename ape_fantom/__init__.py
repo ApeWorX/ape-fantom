7c6652933238a0fb22508eababc30e75668501c0
El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
2022-01-22 19:44:18 -0500

    feat: initial plugin

---
+++ ape_fantom/__init__.py
@@ -0,0 +1,34 @@
+from ape import plugins
+from ape.api import NetworkAPI, create_network_type
+from ape.api.networks import LOCAL_NETWORK_NAME
+from ape_geth import GethProvider
+from ape_test import LocalProvider
+
+from .ecosystem import NETWORKS, Fantom, FantomConfig
+
+
+@plugins.register(plugins.Config)
+def config_class():
+    return FantomConfig
+
+
+@plugins.register(plugins.EcosystemPlugin)
+def ecosystems():
+    yield Fantom
+
+
+@plugins.register(plugins.NetworkPlugin)
+def networks():
+    for network_name, network_params in NETWORKS.items():
+        yield "fantom", network_name, create_network_type(*network_params)
+
+    # NOTE: This works for development providers, as they get chain_id from themselves
+    yield "fantom", LOCAL_NETWORK_NAME, NetworkAPI
+
+
+@plugins.register(plugins.ProviderPlugin)
+def providers():
+    for network_name in NETWORKS:
+        yield "fantom", network_name, GethProvider
+
+    yield "fantom", LOCAL_NETWORK_NAME, LocalProvider
---
+++ ape_fantom/ecosystem.py
@@ -0,0 +1,22 @@
+from ape.api.config import ConfigItem
+from ape.api.networks import LOCAL_NETWORK_NAME
+from ape_ethereum.ecosystem import Ethereum, NetworkConfig
+
+NETWORKS = {
+    # chain_id, network_id
+    "opera": (250, 250),
+    "testnet": (4002, 4002),
+}
+
+
+class FantomConfig(ConfigItem):
+    opera: NetworkConfig = NetworkConfig(required_confirmations=1, block_time=1)  # type: ignore
+    testnet: NetworkConfig = NetworkConfig(required_confirmations=1, block_time=1)  # type: ignore
+    local: NetworkConfig = NetworkConfig(default_provider="test")  # type: ignore
+    default_network: str = LOCAL_NETWORK_NAME
+
+
+class Fantom(Ethereum):
+    @property
+    def config(self) -> FantomConfig:  # type: ignore
+        return self.config_manager.get_config("fantom")  # type: ignore
