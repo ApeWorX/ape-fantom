c3503f6317d3166724ab75ca7b1b8bc12540cc99
unparalleled-js <yingthi@live.com>
2022-03-04 12:27:52 -0600

    chore: migrate pydantic

--- ape_fantom/ecosystem.py
+++ ape_fantom/ecosystem.py
@@ -1,4 +1,4 @@
-from ape.api.config import ConfigItem
+from ape.api.config import PluginConfig
 from ape.api.networks import LOCAL_NETWORK_NAME
 from ape_ethereum.ecosystem import Ethereum, NetworkConfig
 
@@ -9,7 +9,7 @@ NETWORKS = {
 }
 
 
-class FantomConfig(ConfigItem):
+class FantomConfig(PluginConfig):
     opera: NetworkConfig = NetworkConfig(required_confirmations=1, block_time=1)  # type: ignore
     testnet: NetworkConfig = NetworkConfig(required_confirmations=1, block_time=1)  # type: ignore
     local: NetworkConfig = NetworkConfig(default_provider="test")  # type: ignore
