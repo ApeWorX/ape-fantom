11e12d6a03ae9da30747f6ef850e6fff7d1f3071
Just some guy <3859395+fubuloubu@users.noreply.github.com>
2022-03-11 16:34:01 -0500

    fix: remove broken test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -27,9 +27,3 @@ def test_basic(accounts, networks):
     with networks.fantom.local.use_provider("test"):
         a = accounts.test_accounts[0]
         a.transfer(a, 100)
-
-
-def test_connection(Contract, networks):
-    with networks.fantom.opera.use_provider("geth"):
-        a = Contract("0x0000000000000000000000000000000000000000")
-        assert a.balance > 915  # NOTE: Anything sent to 0x0 is lost forever
