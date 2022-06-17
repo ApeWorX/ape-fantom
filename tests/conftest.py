0986fbd805acabccf7b5fc70c6672779b6395514
unparalleled-js <yingthi@live.com>
2022-06-17 08:33:21 -0500

    chore: more fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
-import pytest
 import ape
+import pytest
+from ape._cli import cli as ape_cli
+from click.testing import CliRunner
 
 
 @pytest.fixture
@@ -11,3 +13,12 @@ def networks():
 def accounts():
     return ape.accounts
 
+
+@pytest.fixture
+def runner():
+    return CliRunner()
+
+
+@pytest.fixture
+def cli():
+    return ape_cli
--- tests/test_provider.py
+++ tests/test_provider.py
@@ -1,4 +1,4 @@
-def test_basic(accounts, networks):
+def test_use_provider(accounts, networks):
     with networks.fantom.local.use_provider("test"):
         account = accounts.test_accounts[0]
-        account.transfer(a, 100)
+        account.transfer(account, 100)
