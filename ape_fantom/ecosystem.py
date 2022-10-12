2c854ea669d216a03eb55949fac2b99b0254be89
Juliya Smith <yingthi@live.com>
2022-10-12 15:04:51 -0500

    feat: add fork for testnet (#16)
    Co-authored-by: NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>

--- ape_fantom/ecosystem.py
+++ ape_fantom/ecosystem.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from ape.api.config import PluginConfig
 from ape.api.networks import LOCAL_NETWORK_NAME
 from ape_ethereum.ecosystem import Ethereum, NetworkConfig
@@ -9,10 +11,27 @@ NETWORKS = {
 }
 
 
+def _create_network_config(
+    required_confirmations: int = 1, block_time: int = 1, **kwargs
+) -> NetworkConfig:
+    # Helper method to isolate `type: ignore` comments.
+    return NetworkConfig(
+        required_confirmations=required_confirmations, block_time=block_time, **kwargs
+    )  # type: ignore
+
+
+def _create_local_config(default_provider: Optional[str] = None) -> NetworkConfig:
+    return _create_network_config(
+        required_confirmations=0, block_time=0, default_provider=default_provider
+    )
+
+
 class FantomConfig(PluginConfig):
-    opera: NetworkConfig = NetworkConfig(required_confirmations=1, block_time=1)  # type: ignore
-    testnet: NetworkConfig = NetworkConfig(required_confirmations=1, block_time=1)  # type: ignore
-    local: NetworkConfig = NetworkConfig(default_provider="test")  # type: ignore
+    opera: NetworkConfig = _create_network_config()
+    opera_fork: NetworkConfig = _create_local_config()
+    testnet: NetworkConfig = _create_network_config()
+    testnet_fork: NetworkConfig = _create_local_config()
+    local: NetworkConfig = _create_local_config(default_provider="test")
     default_network: str = LOCAL_NETWORK_NAME
 
 
